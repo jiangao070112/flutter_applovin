7b4126ab1186f76391b80654e28a876c8f5be3ce
jiangao <jiangao070112ja@hotmail.com>
2019-07-23 17:47:22 +0800

    添加代码

--- lib/flutter_applovin.dart
+++ lib/flutter_applovin.dart
@@ -5,6 +5,11 @@ import 'package:flutter/services.dart';
 class FlutterApplovin {
   static const MethodChannel _channel = const MethodChannel('flutter_applovin');
 
+  static Future<String> get loadInterstitial async {
+    final String version = await _channel.invokeMethod('loadInterstitial');
+    return version;
+  }
+  
   static Future<String> get showInterstitial async {
     final String version = await _channel.invokeMethod('interstitial');
     return version;
