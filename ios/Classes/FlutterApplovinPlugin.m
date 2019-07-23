fef30077523e819156e048601903a479958cafa3
jiangao <jiangao070112ja@hotmail.com>
2019-07-23 17:11:39 +0800

    fix bug for build

--- ios/Classes/FlutterApplovinPlugin.m
+++ ios/Classes/FlutterApplovinPlugin.m
@@ -3,7 +3,9 @@
 
 
 @interface FlutterApplovinPlugin() <ALAdLoadDelegate>
+
 @property (nonatomic, strong) ALAd *ad;
+@property (nonatomic, copy) FlutterResult loadedResult;
 @end
 
 @implementation FlutterApplovinPlugin
@@ -20,33 +22,33 @@
 {
     // We now have an interstitial ad we can show!
     self.ad = ad;
+    self.loadedResult(@"true");
 }
 
 - (void)adService:(nonnull ALAdService *)adService didFailToLoadAdWithError:(int)code
 {
     // Look at ALErrorCodes.h for the list of error codes.
+    self.loadedResult(@"false");
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_applovin"
-            binaryMessenger:[registrar messenger]];
-  FlutterApplovinPlugin* instance = [[FlutterApplovinPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_applovin"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterApplovinPlugin* instance = [[FlutterApplovinPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
     [ALSdk initializeSdk];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    switch(call.method) {
-        case "loadInterstitial":
-            [self loadInterstitialAd];
-            break;
-        case "showInterstitial":
-            [[ALInterstitialAd shared] showOver: [UIApplication sharedApplication].keyWindow andRender: self.ad];
-            break;
-        default:
-            result(FlutterMethodNotImplemented);
-            break;
+    
+    if ([call.method isEqualToString:@"loadInterstitial"]){
+        [self loadInterstitialAd];
+        self.loadedResult = result;
+    }else if ([call.method isEqualToString:@"showInterstitial"]){
+        [[ALInterstitialAd shared] showOver: [UIApplication sharedApplication].keyWindow andRender: self.ad];
+    }else {
+        result(FlutterMethodNotImplemented);
     }
 }
 
